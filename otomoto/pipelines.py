e255e29badaad453c598bc85c00ea4785ee69207
Dzordanq <random05510@gmail.com>
2020-02-21 15:10:10 +0100

    Fix class names

--- otomoto/pipelines.py
+++ otomoto/pipelines.py
@@ -6,6 +6,6 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class ScrapyTestingPipeline(object):
+class OtomotoPipeline(object):
     def process_item(self, item, spider):
         return item
